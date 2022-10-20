e855603aa47dd67f83925652bf508523a42368f7
GuilemTheGolem <113972554+GuilemTheGolem@users.noreply.github.com>
2022-10-20 10:05:36 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -4,3 +4,19 @@ Crea una lista e inicializala con 5 cadenas de caracteres leídas por teclado.
 Copia los elementos de la lista en otra lista pero en orden inverso, y muestra sus elementos por la pantalla.
 '''
 
+
+def lista5cadenacaracteres():
+
+    lst = []
+    for i in range(0,5):
+         cadena = input("Dime una cadena de caracteres:")
+         lst.append(cadena)         
+    return lst
+
+lst = lista5cadenacaracteres()
+lst2 = lst.copy()
+lst2.reverse()
+
+
+print("lst", lst)
+print("lst2", lst2)
