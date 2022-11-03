90f86803d6af336548e383ffcb815b8949a044eb
GuilemTheGolem <113972554+GuilemTheGolem@users.noreply.github.com>
2022-11-03 10:06:21 +0100

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,3 +1,4 @@
+import random
 # Ejercicio 9 #
 
 '''
@@ -12,12 +13,55 @@ Implementar la siguientes funciones
 # Dado una lista con números enteros devolver la suma de todos ellos por ejemplo:
               # sumaVector([1,2,3]) -->  6
               # sumaVector([3,4]) --> 7
-def sumaVector(lst):
-  pass
+
 
 
 # Dado una lista con listas de números enteros devolver la suma de todos ellos por ejemplo:
               # sumaVectorDeVector([[1,2],[3,4],[5,6,7]]) --> 28
 
+import random
+
+lst = [0, 1, 3, 5]
+def sumaLista(lst):
+    sumaTotal = 0
+    for i in lst:
+        sumaTotal = sumaTotal + i
+    return sumaTotal
+        
+    
+
+
+      
+              
+
+def generarListaRandom():
+    lst = []
+    for i in range(0,random.randint(0,10)):
+        lst.append(random.randint(0, 50))
+    return lst
+
+def generarListadeListaRandom():
+    lst = []
+    for i in range(0, random.randint(0,5)):
+        lst.append(generarListaRandom())
+    return lst
+
+
+def sumaVector(lst):
+ 
+    sumaTotal = 0
+    for i in range(0, len(lst)):
+    
+        sumaTotal = sumaTotal + lst[i]
+   
+    return sumaTotal
+
+
+
 def sumaVectorDeVector(lst):
-  pass
+    resultado = 0
+    
+    for i in lst:
+        resultado = resultado + sumaVector(i)
+    
+    return resultado
